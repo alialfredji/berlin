14e01e03def30992a83c398d16336aca27e35c50
alialfredji <alfredjiali@gmail.com>
2019-10-08 00:30:33 +0200

    setup

--- src/boot.js
+++ src/boot.js
@@ -15,14 +15,31 @@ export default createHookApp({
         require('@forrestjs/service-express-graphql'),
         require('@forrestjs/service-express-graphql-test'),
         require('@forrestjs/service-express-session'),
+        require('@forrestjs/service-postgres'),
     ],
-    features: [],
+    features: [
+        require('./features/feature-pg-auth'),
+        require('./features/feature-pg-session'),
+        // require('@forrestjs/feature-pg-session-info'),
+    ],
     settings: async ({ setConfig, getEnv, getConfig }) => {
+        setConfig('express.session.duration', '30d')
+
         setConfig('jwt', {
             secret: getEnv('JWT_SECRET'),
             duration: getEnv('JWT_DURATION'),
         })
 
-        // console.log(getConfig('jwt'))
+        setConfig('postgres.connections', [{
+            connectionName: 'default',
+            host: getEnv('PG_HOST'),
+            port: getEnv('PG_PORT'),
+            database: getEnv('PG_DATABASE'),
+            username: getEnv('PG_USERNAME'),
+            password: getEnv('PG_PASSWORD'),
+            maxAttempts: Number(getEnv('PG_MAX_CONN_ATTEMPTS', 25)),
+            attemptDelay: Number(getEnv('PG_CONN_ATTEMPTS_DELAY', 5000)),
+            models: [],
+        }])
     },
 })
